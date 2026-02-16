60030c56f487804c0b54ae21bcf34336d1a380b2
md-nasim-mondal <nasim.programmer24@gmail.com>
2026-02-16 11:03:11 +0600

    update server res status

--- server/src/app.ts
+++ server/src/app.ts
@@ -23,7 +23,7 @@ app.use("/api/v1", router);
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
-    message: "Welcome to Parcel Delivery Management System Server!",
+    message: "Welcome to Care Guide Note App Server!",
   });
 });
 
